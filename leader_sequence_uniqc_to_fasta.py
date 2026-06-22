8c4a44b3f5334934d75b0eb4cc4663993fcad6fa
wrf <wrfbiolum@protonmail.com>
2026-06-22 12:53:10 +0700

    note the counts of those removed

--- leader_sequence_uniqc_to_fasta.py
+++ leader_sequence_uniqc_to_fasta.py
@@ -19,7 +19,10 @@ GAGTTTTAATACTTTCAACACTACTATATAACAAATAATTTGAGG
 NOTE: names of sequences will be unique, but the sequences may not be
 
 Usage examples:
-gzip -dc Bolinopsis_Isoseq.fasta.gz | grep -v ">" | cut -c 1-45 | sort | uniq -c | sort -nr | head -n 40 | leader_sequence_uniqc_to_fasta.py --prefix Bolinopsis > Bolinopsis_Isoseq.leader_sequences.fasta
+gzip -dc Bolinopsis_Isoseq.fasta.gz | grep -v ">" | cut -c 1-45 | sort | uniq -c | sort -nr | head -n 100 | leader_sequence_uniqc_to_fasta.py --prefix Bolinopsis > Bolinopsis_Isoseq.leader_sequences.fasta
+
+if the uniq -c output was already captured, this can be accepted by piping:
+cat leader_seq.counts | ~/git/splice-leader/leader_sequence_uniqc_to_fasta.py -m 100 --prefix Bmicroptera
 """
 
 import sys
@@ -71,8 +74,10 @@ def main():
 
 	n_lines = 0
 	n_written = 0
+	n_written_sum = 0
 	revcomp_count = 0
-	min_remove_count = 0
+	min_remove_count = 0 # n sequences removed
+	min_remove_sum = 0 # total occurrences of the removed N sequences
 	short_error_count = 0
 	for line in sys.stdin:
 		count, item = parse_line(line)
@@ -81,6 +86,7 @@ def main():
 			continue
 		if count < args.minimum_count: # below -m count
 			min_remove_count += 1
+			min_remove_sum += count
 			continue
 		if len(item) < 3: # seq was mostly empty - sometimes A T C or G alone
 			short_error_count += 1
@@ -94,12 +100,14 @@ def main():
 
 		write_fasta(sys.stdout, header, item)
 		n_written += 1
+		n_written_sum += count
 
-	print(f"Read {n_lines} lines, wrote {n_written} FASTA records.", file=sys.stderr)
+	print(f"Read {n_lines} lines, wrote {n_written} FASTA records, with total of {n_written_sum}.", file=sys.stderr)
 	if revcomp_count:
 		print(f"Found {revcomp_count} sequences likely should be reverse complement.", file=sys.stderr)
 	if min_remove_count:
-		print(f"Skipped {min_remove_count} sequences with count less than {args.minimum_count}.", file=sys.stderr)
+		min_remove_pct = 100.0*min_remove_sum/n_written_sum
+		print(f"Skipped {min_remove_count} sequences with count less than {args.minimum_count}, for {min_remove_sum} total ({min_remove_pct:.2f}%).", file=sys.stderr)
 	if short_error_count:
 		print(f"Ignored {short_error_count} sequences under length of 3bp.", file=sys.stderr)
 
